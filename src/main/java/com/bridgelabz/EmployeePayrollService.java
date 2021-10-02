f9eac43c1f35255259fce9098f5aea141d3c1971
vamsikrish-A <krishnavamsi165@gmail.com>
2021-10-02 22:39:23 +0530

    creating a FileIO & Storing data

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
-    //public enum IOService {CONSOLE_IO, FILE_IO, DB_IO, REST_IO}
+    public enum IOService {CONSOLE_IO, FILE_IO, DB_IO, REST_IO}
     private List<EmployeePayrollData> employeePayrollList;
 
     public EmployeePayrollService() {}
@@ -19,7 +19,8 @@ public class EmployeePayrollService {
         EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
         Scanner consoleInputReader = new Scanner(System.in);
         employeePayrollService.readEmployeePayrollData(consoleInputReader);
-        employeePayrollService.writeEmployeePayrollData();
+        IOService ioServices;
+        employeePayrollService.writeEmployeePayrollData(IOService.CONSOLE_IO);
     }
 
     private void readEmployeePayrollData(Scanner consoleInputReader) {
@@ -32,8 +33,11 @@ public class EmployeePayrollService {
         employeePayrollList.add(new EmployeePayrollData(id, name, salary));
     }
 
-    private void writeEmployeePayrollData() {
-        System.out.println("\nWriting Employee Payroll Roaster to Console\n" +employeePayrollList);
+    public void writeEmployeePayrollData(IOService ioService) {
+        if (ioService.equals(IOService.CONSOLE_IO))
+            System.out.println("\nWriting Employee Payroll Roaster to Console\n" +employeePayrollList);
+        else if (ioService.equals(IOService.FILE_IO))
+            new EmployeePayrollFileIOService().writeData(employeePayrollList);
     }
 
 }
